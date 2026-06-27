5d921d853dbe6a7ce3cbbc82ffc1f2e5fd484fe2
meteprathamesh85 <meteprathamesh85@gmail.com>
2026-06-27 14:40:06 +0530

    DOM

---
+++ DOM.js
@@ -0,0 +1,62 @@
+// console.log();
+
+// document.body;
+
+const bodyBackground = document.body;
+// bodyBackground.style.backgroundColor = "green"
+
+// first approach
+// const box4 = document.getElementById('box-4');
+// console.log(box4);
+
+// second approach
+// const div = document.getElementsByTagName("div");
+// console.log(div);
+
+// third approach
+// const boxes = document.getElementsByClassName('box');
+// console.log(boxes);
+
+// modifying html
+const box1 = document.getElementById('box-1');
+// box1.innerHTML = "<b>Prathamesh</b>"
+// box1.innerText = "Prathamesh"
+
+// document.getElementById('box-1').innerText = "Prathamesh"
+// box1.style.backgroundColor = "red";
+
+// const box2 = document.getElementById('box-2');
+// box2.classList.add('circle')
+
+// const box3 = document.getElementById('box-3');
+// box3.classList.remove('circle')
+
+// const boxes = document.getElementsByClassName('box');
+
+// for (let i = 0; i<boxes.length;i++){
+//     boxes[i].classList.add('circle')
+// }
+
+// box3.classList.remove('box')
+
+// creating a new element 
+
+// 1st is to get access
+// 2nd is to contents
+// 3rd is appendeing
+
+// const newPara = document.createElement('p');
+// newPara.innerText = "This is our new para"
+
+// const cont = document.getElementById('container');
+// cont.appendChild(newPara)
+
+// newPara.classList.add('box')
+// newPara.classList.remove('circle')
+// newPara.classList.add('circle')
+
+// const box1=document.getElementById("box-1");
+
+// box1.classList.remove("circle");
+// box1.classList.add("circle");
+
--- sample3/promises.js
+++ sample3/promises.js
@@ -14,27 +14,205 @@
 //     console.log("this will be executed all time");
 // })
 
-function getCandies (callback1) {
+// function getCandies (callback1) {
+//     return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const candies = "🍬"
+//         console.log("here is our candie",candies);
+//         // return candies
+//         // callback1(candies)
+//         resolve(candies)
+//     }, 3000);
+//     })
+// }
+// console.log(getCandies());
+// // pending fullfill rejected 3 promises op
+
+// async function xyz(){
+//     try{
+//         const xyz = await Abc();
+//         console.log();
+        
+//     }
+//     catch{
+
+//     }
+// }
+// console.log("Start");
+
+// async function demo(){
+//     console.log("A");
+// }
+
+// demo();
+
+// console.log("End");
+
+// async function getCandies() {
+//     try{
+//         const keys = await HandoverKeys(candies)
+//         console.log();
+        
+//         const boarded = await onboarding(keys)
+//         console.log();
+
+//         const result = await result(boarded)
+//         console.log(result);
+//     }
+//     catch(error) {
+//         console.log("rerror occured");
+//     }
+// }
+
+// const createTicket = new Promise(function (resolve, reject){
+//     const isbooked = true
+//     if(isbooked){
+//         resolve("Ticket confirmed 🎫")
+//     }else{
+//         reject("Booking failed ❌")
+//     }
+// })
+
+// createTicket.then((data) => {
+//     console.log("success : Ticket confirmed 🎫");
+// }).catch((data) => {
+//     console.log("Error : bookinng failed ❌");
+// }).finally((data) => {
+//     console.log("Welcome to ticket booking");
+    
+// })
+
+// const login = new Promise(function (resolve, reject){
+//     const islogin = true
+//     if(islogin){
+//         resolve("user logged in 👤")
+//     }else{
+//         reject("user not logged in 👤")
+//     }
+// })
+
+// const getProfile = new Promise(function (resolve, reject){
+//     const isgetProfile = true
+//     if(isgetProfile){
+//         resolve("Profile Loaded 📄")
+//     }else{
+//         reject("Profile not loaded 📄")
+//     }
+// })
+
+// const showDashboard = new Promise(function (resolve, reject){
+//     const isshowDashboard = true
+//     if(isshowDashboard){
+//         resolve("dashboard opened 🖥️")
+//     }else{
+//         reject("Dashboard failed to open 🖥️")
+//     }
+// })
+
+// async function Async() {
+//     try {
+//         const loggedin = await login
+//         console.log(loggedin);
+
+//         const profile = await getProfile
+//         console.log(profile);
+
+//         const dashboard = await showDashboard
+//         console.log(dashboard);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// Async();
+
+// login
+//     .then((data) => {
+//         console.log(data);
+//         return getProfile;
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         return showDashboard;
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+
+
+function order (callback1) {
     return new Promise((resolve, reject) => {
     setTimeout(() => {
-        const candies = "🍬"
-        console.log("here is our candie",candies);
-        // return candies
-        // callback1(candies)
-        resolve(candies)
+        const pizza = "🍕"
+        console.log("here is our pizza",pizza);
+        resolve()
+    }, 5000);
+    })
+}
+
+function payment (callback2) {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const payment = "💳"
+        console.log("Your payment is successfull",payment);
+        resolve()
     }, 3000);
     })
 }
-console.log(getCandies());
-// pending fullfill rejected 3 promises op
-
-async function xyz(){
-    try{
-        const xyz = await Abc();
-        console.log();
-        
-    }
-    catch{
-        
-    }
+function preparing (callback3) {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const preparing = "👩🏻‍🍳"
+        console.log("your pizza is preparing",preparing);
+        resolve()
+    }, 4000);
+    })
 }
+
+function delivery (callback4) {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const delivery = "🚚"
+        console.log("your pizza is out for dwelivery",delivery);
+        resolve()
+    }, 5000);
+    })
+}
+
+function delivered (callback5) {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const delivered = "🏡"
+        console.log("your pizza is delivered",delivered);
+        resolve()
+    }, 4000);
+    })
+}
+
+async function fooddelivery() {
+    try{
+    const Order = await order()
+    console.log(Order);
+
+    const Payment = await payment()
+    console.log(Payment);
+
+    const prepare = await preparing()
+    console.log(prepare);
+
+    const out = await delivery()
+    console.log(out);
+
+    const home = await delivered()
+    console.log(home);
+    
+    }
+    catch(error) {
+        console.log("Error : Something wrong "); 
+    }     
+}
+
+fooddelivery()
